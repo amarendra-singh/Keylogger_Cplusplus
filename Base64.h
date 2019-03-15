ca4c7e47351af57d1f45d00144b9916de6a93561
Amarendra Singh <226+amarendra-singh@users.noreply.github.com>
2019-03-15 13:18:28 +0530

    Completed

--- Base64.h
+++ Base64.h
@@ -17,19 +17,19 @@ const std::string &SALT3 = "line=wowC++";
 std::string EncryptB64 (std::string s)
     {
         s = SALT1 + s + SALT2 + SALT3;
-        s = base64_encode (s); 
+        s = base64_encode (s);
         s.insert (7, SALT3);
         s += SALT1;
         s = base64_encode (s);
         s = SALT2 + SALT3 + s + SALT1;
         s = base64_encode (s);
-        s.insert (1, "L"); 
-        s.insert (7, "M"); 
+        s.insert (1, "L");
+        s.insert (7, "M");
         return s;
     }
 std::string DecryptB64 (std::string s)
     {
-        s = s.erase (7, 1); 
+        s = s.erase (7, 1);
         s = s.erase (1, 1);
         s = base64_decode (s);
         s = s.substr (SALT2.length() + SALT3.length());
@@ -50,7 +50,7 @@ std::string base64_encode (const std::string &s)
         std::string ret;
         int val = 0;
         int bits = -6;
-        const unsigned int b63 = 0x3F; 
+        const unsigned int b63 = 0x3F;
         for (const auto &c : s)
             {
                 val = (val << 8) + c;
@@ -61,7 +61,7 @@ std::string base64_encode (const std::string &s)
                         bits -= 6;
                     }
             }
-        if (bits > -6) 
+        if (bits > -6)
             ret.push_back(BASE64_CODES[((val << 8) >> (bits + 8)) & b63]);
 
         while (ret.size() % 4)
@@ -76,22 +76,22 @@ std::string base64_decode(const std::string &s)
         std::vector<int> vec(256, -1);
         for (int i = 0; i < 64; i++)
             vec [BASE64_CODES[i]] = i;
-        int val = 0, bits = -8; 
+        int val = 0, bits = -8;
         for (const auto &c : s)
             {
                 if (vec[c] == -1) break;
-                val = (val << 6) + vec[c]; 
-                bits += 6; 
-                           
+                val = (val << 6) + vec[c];
+                bits += 6;
+
                 if (bits >= 0)
                     {
 
                         ret.push_back(char((val >> bits) & 0xFF));
-                        bits -= 8; 
+                        bits -= 8;
                     }
             }
 
         return ret;
     }
 }
-#endif 
+#endif
